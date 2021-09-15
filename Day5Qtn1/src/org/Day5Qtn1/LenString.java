e81dac52481b9c7ead72728eb91c5e6d743948d2
AdityaTorpe <47110226+AdityaTorpe@users.noreply.github.com>
2021-09-15 18:22:04 +0530

    Day5Qtn1

---
+++ Day5Qtn1/src/org/Day5Qtn1/LenString.java
@@ -0,0 +1,16 @@
+package org.Day5Qtn1;
+public class LenString {
+	public static void main(String[] args) {
+		String str = "Technology";
+		System.out.println("Technology length is:"  +str.length());
+		
+		String str2 = "SeleniumAutomationtool";
+		System.out.println("SeleniumAutomationtool length is:"  +str2.length());
+		
+		String str3 = "j a v a p r o g r a m";
+		System.out.println("Technology length is:"  +str3.length());
+		
+				
+	}
+	
+}
